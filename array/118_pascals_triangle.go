e6465b1bcf669efc42293dc9839d4b78213ba135
wangsiwei <sw.cumt.1993@gmail.com>
2021-11-09 16:39:26 +0800

    feat(solution): add leetcode 188 Pascal's Triangle

---
+++ array/118_pascals_triangle.go
@@ -0,0 +1,38 @@
+package array
+
+/**
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+
+
+Constraints:
+
+1 <= numRows <= 30
+*/
+
+func GenerateTriangle(numRows int) [][]int {
+	res := make([][]int, numRows)
+	res[0] = []int{1}
+	for i := 1; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
+}
